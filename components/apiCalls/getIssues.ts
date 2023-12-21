b174a700acd345951684840de18ac96cd7f7ac35
ratan9333 <107601838+ratan9333@users.noreply.github.com>
2023-12-21 09:26:00 +0530

    Add files via upload

--- components/apiCalls/getIssues.ts
+++ components/apiCalls/getIssues.ts
@@ -1,9 +1,9 @@
 import axios from "axios";
 
-export const URL = "http://localhost:3003";
-
+export const URL = "https://express-api-1ycv.onrender.com";
 export async function getIssues(status: string) {
   const url = URL + "/issues";
+  console.log({ small: url });
   try {
     const response = await axios.post(url, {
       status,
